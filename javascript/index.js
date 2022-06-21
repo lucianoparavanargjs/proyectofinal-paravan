cb270947979d96c6ed89241b56c6f2f9748ec716
lucianoparavanargjs <107894000+lucianoparavanargjs@users.noreply.github.com>
2022-06-20 23:30:22 -0300

    desafio complementario

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+let entrada = prompt("Ingresar un número: ");
+
+while(entrada != "ESC"){
+    alert("El usuario ingresó el número: " + entrada);
+    
+    entrada = prompt("Ingresar un número: ")
+}
