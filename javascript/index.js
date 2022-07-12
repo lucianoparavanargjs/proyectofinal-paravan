474c89511ba9369167f1cf4805c1267d7c7a5326
lucianoparavanargjs <107894000+lucianoparavanargjs@users.noreply.github.com>
2022-07-12 00:05:42 -0300

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,63 +1,101 @@
-let miCompra = []
-const productos = [
-    {id: 1, nombre: "producto1", precio: 5000},
-    {id: 2, nombre: "producto2", precio: 10000},
-    {id: 3, nombre: "producto3", precio: 15000}
-]
-
-function mostrarMenuProductos(){
-    let idElegido = prompt("Escriba el id del producto elegido. Para salir ingrese (ESC): \n Producto 1: id=1 \n Producto 2: id=2 \n Producto 3: id=3 \n Una vez que haya finalizado escriba `TOTAL` para conocer el precio final de su compra \n")
-    return idElegido 
-}
-
-function cantidadProductos(){
-    let cantidadProductos = prompt("Escriba el número de unidades que desea")
-    return cantidadProductos
-}
-
-function totalProducto1(){
-    let unidades = prompt("Escriba el numero de unidades de Producto1 que desea: ")
-    return miCompra.push(productos[0].precio*unidades)
-}
-
-function totalProducto2(){
-    let unidades = prompt("Escriba el numero de unidades de Producto2 que desea: ")
-    return miCompra.push(productos[1].precio*unidades)
-}
-
-function totalProducto3(){
-    let unidades = prompt("Escriba el numero de unidades de Producto1 que desea: ")
-    return miCompra.push(productos[2].precio*unidades)
-}
-
-function totalProductos(){
-    let total = miCompra.reduce((acc, el) => acc + el, 0)
-    return total
-}
-
-function carritoCompras(){
-    let idElegido = mostrarMenuProductos()
-
-    while(idElegido !== "ESC" && idElegido !== ""){
-        if (idElegido == 1){
-            totalProducto1()
-        }
-        else if(idElegido == 2){
-            totalProducto2()
-        }
-        else if(idElegido == 3){
-            totalProducto3()
-        }
-        else if(idElegido == "TOTAL"){
-            let total = totalProductos()
-            alert("El total es $" + total)
-            break
-        }
-        else{
-            alert("Debe elegir alguno de los id del listado")
-        }
-        idElegido = mostrarMenuProductos()
+class Producto {
+    constructor(id, nombre, imagen, precioProducto) {
+        this.id = id
+        this.nombre = nombre
+        this.imagen = imagen
+        this.precioProducto = precioProducto
     }
 }
 
-carritoCompras()
+class Carrito {
+    constructor(id) {
+        this.id = id;
+        this.productos = []
+    }
+
+    calcularTotal() {
+        let total = 0
+        for(let i = 0; i < this.productos.length; i++) {
+            total = total + this.productos[i].precioKilo
+        }
+        return total
+    }
+
+}
+
+function renderCard(producto) {
+    let cardRendered = `    
+    <div class="card m-3" style="width: 18rem;">
+        <img src="./images/${producto.imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${producto.id}.${producto.nombre}</h5>
+            <p class="card-text">$ ${producto.precioProducto}</p>
+            <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carrito</a>
+        </div>
+    </div>
+    `
+    return cardRendered
+}
+
+function actualizarCarrito(carrito) {
+    let divCarrito = document.querySelector("#carrito")
+    carrito.productos.forEach(producto => {
+        divCarrito.innerHTML += renderCard(producto)
+    })
+    divCarrito.innerHTML += `<h1>Precio Total: $ ${carrito.calcularTotal()}</h1>`
+}
+
+let catalogoProductos = [];
+let producto1 = new Producto(1, "producto1", "adidas1.jpg", 20000)
+let producto2 = new Producto(2, "producto2", "adidas2.jpg", 25000)
+let producto3 = new Producto(3, "producto3", "adidas3.jpg", 27000)
+
+catalogoProductos.push(producto1)
+catalogoProductos.push(producto2)
+catalogoProductos.push(producto3)
+
+let cardsDiv = document.querySelector("#cards")
+catalogoProductos.forEach(producto => {
+    cardsDiv.innerHTML += renderCard(producto)
+})
+
+let miFormulario = document.getElementById("formulario")
+let formulario = document.getElementById("formulario")
+let inputNombre = document.getElementById("inputIdProducto")
+let inputCantidad = document.getElementById("inputCantidad")
+
+let productos = []
+
+formulario.onsubmit = (event) => validarFormulario(event)
+
+function validarFormulario(event) {
+    event.preventDefault()
+    console.log(event.target)
+    let id = inputIdProducto.value
+    let cantidad = parseInt(inputCantidad.value)
+    let precioUnitario
+    if (id == 1){
+        precioUnitario = 20000
+    }
+    else if(idElegido == 2){
+        precioUnitario = 25000
+    }
+    else if(idElegido == 3){
+        precioUnitario = 27000
+    }
+    let totalPrecio = cantidad * precioUnitario
+  
+    let producto = new Productos(
+      id,
+      cantidad,
+      totalPrecio,
+    )
+
+    productos.push(producto);
+  
+    formulario.reset();
+
+  console.log(inputIdProducto.value);
+  console.log(inputCantidad.value);
+  console.log(productos);
+}
