9455a07b15a4fc9b0bf30c4c217ae05d915c7a40
lucianoparavanargjs <107894000+lucianoparavanargjs@users.noreply.github.com>
2022-07-18 18:20:40 -0300

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,101 +1,137 @@
 class Producto {
-    constructor(id, nombre, imagen, precioProducto) {
+    constructor(id, nombre, precioProducto, imagen){
         this.id = id
         this.nombre = nombre
-        this.imagen = imagen
         this.precioProducto = precioProducto
+        this.imagen = imagen
     }
 }
 
-class Carrito {
-    constructor(id) {
-        this.id = id;
-        this.productos = []
+class ProductoCompra {
+    constructor(id, nombre, precioProducto, unidades, totalProducto){
+        this.id = id
+        this.nombre = nombre
+        this.precioProducto = precioProducto
+        this.unidades = unidades
+        this.totalProducto = precioProducto * unidades
     }
-
-    calcularTotal() {
-        let total = 0
-        for(let i = 0; i < this.productos.length; i++) {
-            total = total + this.productos[i].precioKilo
-        }
-        return total
-    }
-
 }
 
-function renderCard(producto) {
-    let cardRendered = `    
-    <div class="card m-3" style="width: 18rem;">
-        <img src="./images/${producto.imagen}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${producto.id}.${producto.nombre}</h5>
-            <p class="card-text">$ ${producto.precioProducto}</p>
-            <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carrito</a>
-        </div>
-    </div>
-    `
-    return cardRendered
-}
 
-function actualizarCarrito(carrito) {
-    let divCarrito = document.querySelector("#carrito")
-    carrito.productos.forEach(producto => {
-        divCarrito.innerHTML += renderCard(producto)
-    })
-    divCarrito.innerHTML += `<h1>Precio Total: $ ${carrito.calcularTotal()}</h1>`
-}
-
-let catalogoProductos = [];
-let producto1 = new Producto(1, "producto1", "adidas1.jpg", 20000)
-let producto2 = new Producto(2, "producto2", "adidas2.jpg", 25000)
-let producto3 = new Producto(3, "producto3", "adidas3.jpg", 27000)
+let catalogoProductos = []
+let producto1 = new Producto(1, "producto1", 20000, "imagenProducto1.jpg")
+let producto2 = new Producto(2, "producto2", 21000, "imagenProducto2.jpg")
+let producto3 = new Producto(3, "producto3", 24000, "imagenProducto3.jpg")
 
 catalogoProductos.push(producto1)
 catalogoProductos.push(producto2)
 catalogoProductos.push(producto3)
 
-let cardsDiv = document.querySelector("#cards")
-catalogoProductos.forEach(producto => {
-    cardsDiv.innerHTML += renderCard(producto)
-})
+const productos = []
+let productoACargar =[]
+let tabla
+let textoTotalCompra
 
-let miFormulario = document.getElementById("formulario")
-let formulario = document.getElementById("formulario")
-let inputNombre = document.getElementById("inputIdProducto")
-let inputCantidad = document.getElementById("inputCantidad")
-
-let productos = []
-
-formulario.onsubmit = (event) => validarFormulario(event)
-
-function validarFormulario(event) {
-    event.preventDefault()
-    console.log(event.target)
-    let id = inputIdProducto.value
-    let cantidad = parseInt(inputCantidad.value)
-    let precioUnitario
-    if (id == 1){
-        precioUnitario = 20000
-    }
-    else if(idElegido == 2){
-        precioUnitario = 25000
-    }
-    else if(idElegido == 3){
-        precioUnitario = 27000
-    }
-    let totalPrecio = cantidad * precioUnitario
-  
-    let producto = new Productos(
-      id,
-      cantidad,
-      totalPrecio,
-    )
-
-    productos.push(producto);
-  
-    formulario.reset();
-
-  console.log(inputIdProducto.value);
-  console.log(inputCantidad.value);
-  console.log(productos);
+function inicializarElementos() {
+    tabla = document.getElementById("tabla-productos")
+    textoTotalCompra = document.querySelector("#totalCompra span")
 }
+
+let idDatoFormulario = document.getElementById("inputIdProducto").value
+let unidadesFormulario = document.getElementById("inputUnidades").value
+
+function inicializarElementos() {
+    tabla = document.getElementById("tabla-productos");
+    textoTotalCompra = document.querySelector("#totalCompra");
+}
+
+let formulario = document.getElementById("formulario")
+
+formulario.onsubmit = (event) => agregarAlCarrito(event)
+
+function agregarAlCarrito(event) {
+    event.preventDefault()
+
+    if (idDatoFormulario == 1) {
+        let agregarId = productoACargar.push(1)
+        let agregarNombre = productoACargar.push(catalogoProductos[0].nombre)
+        let agregarPrecio = productoACargar.push(catalogoProductos[0].precioProducto)
+        let agregarUnidades = productoACargar.push(unidadesFormulario)
+        let total = productoACargar.push(agregarPrecio * agregarUnidades)
+        let productoARegistrar = new Producto(
+            agregarId,
+            agregarNombre,
+            agregarPrecio,
+            agregarUnidades,
+            total
+        )
+        productoACargar.push(productoARegistrar)
+    }
+    else if (idDatoFormulario == 2) {
+        let agregarId = productoACargar.push(2)
+        let agregarNombre = productoACargar.push(catalogoProductos[1].nombre)
+        let agregarPrecio = productoACargar.push(catalogoProductos[1].precioProducto)
+        let agregarUnidades = productoACargar.push(unidadesFormulario)
+        let total = productoACargar.push(agregarPrecio * agregarUnidades)
+        let productoARegistrar = new Producto(
+            agregarId,
+            agregarNombre,
+            agregarPrecio,
+            agregarUnidades,
+            total
+        )
+        productoACargar.push(productoARegistrar)
+    }
+    else if (idDatoFormulario == 3) {
+        let agregarId = productoACargar.push(3)
+        let agregarNombre = productoACargar.push(catalogoProductos[2].nombre)
+        let agregarPrecio = productoACargar.push(catalogoProductos[2].precioProducto)
+        let agregarUnidades = productoACargar.push(unidadesFormulario)
+        let total = productoACargar.push(agregarPrecio * agregarUnidades)
+        let productoARegistrar = new Producto(
+            agregarId,
+            agregarNombre,
+            agregarPrecio,
+            agregarUnidades,
+            total
+        )
+        productoACargar.push(productoARegistrar)
+    }
+}
+
+
+function agregarProductosTabla() {
+    
+    productoACargar.forEach( (producto) => {
+        let filaTabla = document.createElement("tr")
+        filaTabla.innerHTML = `
+        <td> ${productoACargar[0]}</td>
+        <td> ${productoACargar[1]}</td>
+        <td> ${productoACargar[2]}</td>
+        <td> ${productoACargar[3]}</td>
+        <td> ${productoACargar[3]}</td>
+
+        `
+        tabla.tBodies[0].append(filaTabla)
+    })
+}
+
+function calcularTotales() {
+
+}
+
+function costoEnvio() {
+    if (totalFinal > 10000){
+        let mensajeEnvio = "Envío gratis"
+    }
+    else {
+        let mensajeEnvio = "Deberá abonar $800 de envío"
+    }
+}
+function main() {
+    inicializarElementos()
+    agregarAlCarrito()
+    agregarProductosTabla()
+}
+
+main()
